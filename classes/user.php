0215cde4d9866607fa69238c2feeb0e15ed5ef68
mihguelsilva <mihguelaraujo@gmail.com>
2024-11-15 18:18:20 -0300

    Starting to create PHP's classes and instance user's sessions

---
+++ classes/user.php
@@ -0,0 +1,33 @@
+<?php
+class User {
+    public function register ($name, $phone, $email, $password) {
+        global $pdo;
+        $sql = $pdo->prepare('SELECT id_user FROM user_register WHERE email = :email');
+        $sql->bindValue(':email', $email);
+        if ($sql->rowCount() > 0) {
+            return false;
+        } else {
+            $password = md5($password);
+            $sql = $pdo->prepare('INSERT INTO user_register (name, phone, email, password) VALUES (:name, :phone, :email, :password)');
+            $sql->bindValue(":name", $name);
+            $sql->bindValue(":phone", $phone);
+            $sql->bindValue(":email", $email);
+            $sql->bindValue(":password", $password);
+            $sql->execute();
+            echo "<script>window.alert('New user registered succesfully!');</script>";
+            header("location: /login.php");
+            return true;
+        }
+    }
+    public function signin($email, $password) {
+        $sql = $pdo->prepare('SELECT id_user FROM user_register WHERE email = :email AND password = :password');
+        $sql->bindValue(":email", $email);
+        $sql->bindValue(":password", $password);
+        $sql->execute();
+        if ($sql->rowCount() > 0) {
+            $data = $sql->fetch();
+            $_SESSION['login'] = $data['id_user'];
+        }
+    }
+}
+?>
--- connect.php
+++ connect.php
@@ -1,6 +1,13 @@
 <?php
+session_start();
 global $pdo;
-
 try {
+    $pdo = new PDO('mysql:dbname=final_project;host=localhost', 'eadccna', 'eadCCn@');
+} catch(PDOException $e) {
+    echo "Database Error: " . $e->getMessage();
+    exit();
+} catch(Exception $e) {
+    echo "Error: " . $e->getMessage();
+    exit();
 }
 ?>
