e8b85e79546585574d839830716f939eacf72f22
mihguelsilva <mihguelaraujo@gmail.com>
2024-11-14 21:57:45 -0300

    Adding main HTML and CSS structure

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+    <head>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+	<title>Project</title>
+	<link rel='preconnect' href='https://fonts.googleapis.com'>
+	<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+	<link href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' rel='stylesheet'>
+	<link href='https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap' rel='stylesheet'>
+	<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong'>
+	<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
+	<link rel="stylesheet" href="/css/main-page.css">
+    </head>
+    <body>
+	<header>
+	    <div class="page-width">
+		<h1>Advertise Here</h1>
+		<h2>Quick and easy sell.</h2>
+		<figure>
+		    <a href="">
+			<img src="/site-images/perfil.png" alt="image's user perfil" width="100%">
+			<figcaption>Sign in</figcaption>
+		    </a>
+		</figure>
+	    </div>
+	</header>
+	<nav id="menu">
+	    <div class="page-width">
+		<div id="sites-name">
+		    <a href="/index.php"><h2>SITE'S NAME</h2></a>
+		</div>
+		<div id="links">
+		    <ul>
+			<li><a href="">Advertise</a></li>
+			<li><a href="">Contact</a></li>
+			<li><a href="">About us</a></li>
+		    </ul>
+		</div>
+	    </div>
+	</nav>
+	<section>
+	    <div class="page-width">
+		<div class="box">
+		    <img src="">
+		    <a href="">
+			<hgroup>
+			    <h3>Title</h3>
+			    <h5>Category</h5>
+			    <h4>Value</h4>
+			</hgroup>
+		    </a>
+		</div>
+		<div class="box">
+		    <img src="">
+		    <a href="">
+			<hgroup>
+			    <h3>Title</h3>
+			    <h5>Category</h5>
+			    <h4>Value</h4>
+			</hgroup>
+		    </a>
+		</div>
+		<div class="box">
+		    <img src="">
+		    <a href="">
+			<hgroup>
+			    <h3>Title</h3>
+			    <h5>Category</h5>
+			    <h4>Value</h4>
+			</hgroup>
+		    </a>
+		</div>
+		<div class="box">
+		    <img src="">
+		    <a href="">
+			<hgroup>
+			    <h3>Title</h3>
+			    <h5>Category</h5>
+			    <h4>Value</h4>
+			</hgroup>
+		    </a>
+		</div>
+		<div class="box">
+		    <img src="">
+		    <a href="">
+			<hgroup>
+			    <h3>Title</h3>
+			    <h5>Category</h5>
+			    <h4>Value</h4>
+			</hgroup>
+		    </a>
+		</div>
+		<div class="box">
+		    <img src="">
+		    <a href="">
+			<hgroup>
+			    <h3>Title</h3>
+			    <h5>Category</h5>
+			    <h4>Value</h4>
+			</hgroup>
+		    </a>
+		</div>
+		<div class="box">
+		    <img src="">
+		    <a href="">
+			<hgroup>
+			    <h3>Title</h3>
+			    <h5>Category</h5>
+			    <h4>Value</h4>
+			</hgroup>
+		    </a>
+		</div>
+	    </div>
+	</section>
+    </body>
+</html>
